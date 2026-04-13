5e64d4ae9ff93b94df15225508c0771f5716a718
JF9SOM <jf9som@gmail.com>
2026-04-13 20:59:40 +0900

    Standardize Time labels and implement date-rollover support across Dashboard, Logbook, and Logbook Advanced

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -338,7 +338,7 @@ function echo_table_header_col($name) {
 							<tr>
 								<th scope="col"><?= __("Date"); ?></th>
 								<?php if(($this->config->item('use_auth') && ($this->session->userdata('user_type') >= 2)) || $this->config->item('use_auth') === FALSE || ($this->config->item('show_time'))) { ?>
-								<th scope="col"><?= (display_qso_time_label() === 'local') ? __("Localtime") : __("Time"); ?></th>
+								<th scope="col"><?= __("Time"); ?></th>
 								<?php } ?>
 								<th scope="col"><?= __("Callsign"); ?></th>
 								<?php
@@ -361,7 +361,7 @@ function echo_table_header_col($name) {
 									$custom_date_format = $this->config->item('qso_date_format');
 								}
 								?>
-								<td><?php $timestamp = strtotime($row->COL_TIME_ON ?? '1970-01-01 00:00:00'); echo date($custom_date_format, $timestamp); ?></td>
+								<td><?php echo display_qso_date($row->COL_TIME_ON ?? '1970-01-01 00:00:00'); ?></td>
 								<?php if(($this->config->item('use_auth') && ($this->session->userdata('user_type') >= 2)) || $this->config->item('use_auth') === FALSE || ($this->config->item('show_time'))) { ?>
 								<td><?php echo display_qso_time($row->COL_TIME_ON ?? '1970-01-01 00:00:00'); ?></td>
 								<?php } ?>
--- application/views/view_log/partial/log_ajax.php
+++ application/views/view_log/partial/log_ajax.php
@@ -133,7 +133,7 @@ function getDistance($distance) {
             <tr class="titles">
                 <th scope="col"><?= __("Date"); ?></th>
                 <?php if(($this->config->item('use_auth') && ($this->session->userdata('user_type') >= 2)) || $this->config->item('use_auth') === FALSE || ($this->config->item('show_time'))) { ?>
-                <th scope="col"><?= (display_qso_time_label() === 'local') ? __("Localtime") : __("Time"); ?></th>
+                <th scope="col"><?= __("Time"); ?></th>
                 <?php } ?>
                 <th scope="col"><?= __("Call"); ?></th>
                 <?php
@@ -182,7 +182,7 @@ function getDistance($distance) {
                     $custom_date_format = $this->config->item('qso_date_format');
                 }
                 echo '<tr class="tr'.($i & 1).'" id="qso_'. $row->COL_PRIMARY_KEY .'">'; ?>
-            <td><?php $timestamp = strtotime($row->COL_TIME_ON ?? '1970-01-01 00:00:00'); echo date($custom_date_format, $timestamp); ?></td>
+            <td><?php echo display_qso_date($row->COL_TIME_ON ?? '1970-01-01 00:00:00'); ?></td>
             <?php if(($this->config->item('use_auth') && ($this->session->userdata('user_type') >= 2)) || $this->config->item('use_auth') === FALSE || ($this->config->item('show_time'))) { ?>
                 <td><?php echo display_qso_time($row->COL_TIME_ON ?? '1970-01-01 00:00:00'); ?></td>
             <?php } ?>
